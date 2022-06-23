d42338bcdd6347e6642472206d5c4ab577d224b5
Vitalya <vitalya.2211@yandex.ru>
2022-06-24 06:26:47 +1000

    create project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace home_work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Напишите программу, 
+          * которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
+            Console.WriteLine("программа принимает два числа, возвращает большее\n Введите первое число:");
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе число:");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+                Console.WriteLine("max= " + a);
+            else if (a < b)
+                Console.WriteLine("max= " + b);
+            else Console.WriteLine("числа равны");
+            Console.ReadKey();
+
+        }
+    }
+}
